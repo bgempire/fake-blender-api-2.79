03d4d5f95aa7ede366a0bf98cf7aa1f2e7500bff
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-02-12 12:09:52 -0300

    feat: update type comments on bge.app

--- bge/app/__init__.py
+++ bge/app/__init__.py
@@ -1,14 +1,15 @@
-"""Module to access application values that remain unchanged during runtime."""
+"""Application Data (bge.app)
 
+Module to access application values that remain unchanged during runtime."""
 
 # Variables
-version = (2, 7, 9) # type: tuple[int]
-version_string = "2.79 (sub 1)" # type: str
-version_char = "2.79" # type: str
-has_texture_ffmpeg = True # type: bool
-has_joystick = True # type: bool
-has_physics = True # type: bool
+version = (2, 7, 9)  # type: tuple[int, int, int]
+version_string = "2.79 (sub 1)"  # type: str
+version_char = "2.79"  # type: str
+has_texture_ffmpeg = True  # type: bool
+has_joystick = True  # type: bool
+has_physics = True  # type: bool
 
 # UPBGE
-upbge_version = (0, 2, 4) # type: tuple[int]
-upbge_version_string = "0.2 (sub 4)" # type: str
+upbge_version = (0, 2, 4)  # type: tuple[int, int, int]
+upbge_version_string = "0.2 (sub 4)"  # type: str
