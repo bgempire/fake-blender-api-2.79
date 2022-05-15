7831ae775431c91c303aadf916540c0f764f12cc
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-05-15 14:59:24 -0300

    feat: add docs and types to mathutils.interpolate

--- mathutils/interpolate.py
+++ mathutils/interpolate.py
@@ -2,11 +2,13 @@
 
 The Blender interpolate module"""
 
+
 def poly_3d_calc(veclist, pt):
-	"""Calculate barycentric weights for a point on a polygon.
+    # type: (list, list) -> list
+    """Calculate barycentric weights for a point on a polygon.
 
-	Parameters:	
-	veclist - list of vectors
-	pt - point :rtype: list of per-vector weights"""
+    Args:
+        veclist: list of vectors
+        pt: point list of per-vector weights"""
 
-	pass
+    pass
