e11a6dfefa3d2de99db9c36c838d36bf8cde1a07
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-05-15 15:07:37 -0300

    feat: add types and docs to mathutils.kdtree

--- mathutils/kdtree.py
+++ mathutils/kdtree.py
@@ -2,56 +2,70 @@
 
 Generic 3-dimentional kd-tree to perform spatial searches."""
 
+from . import Vector as _Vector
+from typing import Callable as _Callable
+
+
 class KDTree:
-	"""KdTree(size) -> new kd-tree initialized to hold size items.
+    """KdTree(size) -> new kd-tree initialized to hold size items.
 
-	Note: KDTree.balance must have been called before using any of the find methods."""
-	
-	def balance(self):
-		"""Balance the tree.
+    Note:
+        KDTree.balance must have been called before using any of the find methods."""
 
-		Note: This builds the entire tree, avoid calling after each insertion."""
-		
-		pass
-		
-	def find(self, co, filter=None):
-		"""Find nearest point to co.
+    def balance(self):
+        # type: () -> None
+        """Balance the tree.
 
-		Parameters:	
-		co (float triplet) - 3d coordinates.
-		filter (callable) - function which takes an index and returns True for indices to include in the search.
-		
-		Returns: Returns (Vector, index, distance).
+        Note:
+            This builds the entire tree, avoid calling after each insertion."""
 
-		Return type: tuple"""
-		
-		from . import Vector
-		return (Vector, 1, 1.0)
-		
-	def find_n(self, co, n):
-		"""Find nearest n points to co.
+        pass
 
-		Parameters:	
-		co (float triplet) - 3d coordinates.
-		n (int) - Number of points to find.
-		
-		Returns: Returns a list of tuples (Vector, index, distance).
+    def find(self, co, filter=None):
+        # type: (list, _Callable) -> tuple[_Vector, int, float]
+        """Find nearest point to co.
 
-		Return type: list"""
-		
-		from . import Vector
-		return [Vector, 1, 1.0]
-		
-	def find_range(self, co, radius):
-		"""Find nearest n points to co.
+        Args:
+            co (float triplet) - 3d coordinates.
+            filter (callable) - function which takes an index and returns True for indices to include in the search.
 
-		Parameters:	
-		co (float triplet) - 3d coordinates.
-		n (int) - Number of points to find.
-		
-		Returns: Returns a list of tuples (Vector, index, distance).
+        Returns:
+            tuple: (Vector, index, distance)."""
 
-		Return type: list"""
-		
-		from . import Vector
-		return [Vector, 1, 1.0]
+        pass
+
+    def find_n(self, co, n):
+        # type: (list, int) -> tuple[_Vector, int, float]
+        """Find nearest n points to co.
+
+        Args:
+            co (float triplet) - 3d coordinates.
+            n (int) - Number of points to find.
+
+        Returns:
+            list: Returns a list of tuples (Vector, index, distance)."""
+
+        pass
+
+    def find_range(self, co, radius):
+        # type: (list, float) -> list[tuple[_Vector, int, float]]
+        """Find all points within radius of co.
+
+        Args:
+            co (float triplet): 3d coordinates.
+            radius (float): Distance to search for points.
+
+        Returns:
+            list: Returns a list of tuples (Vector, index, distance)."""
+
+        pass
+
+    def insert(self, co, index):
+        # type: (list, int) -> None
+        """Insert a point into the KDTree.
+
+        Args:
+            co (float triplet): Point 3d position.
+            index (int): The index of the point."""
+
+        pass
