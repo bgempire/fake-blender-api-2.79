700884a1743b0c8e1fc32a8437602d6a5712fc26
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-05-15 10:07:36 -0300

    feat: update bge.constraints

--- bge/constraints/__init__.py
+++ bge/constraints/__init__.py
@@ -1,224 +1,291 @@
 """Module to create and control physics constraints."""
 
+from .. import types as _types
+
 
 ## Functions
 def createConstraint(physicsid_1, physicsid_2, constraint_type, pivot_x=0.0, pivot_y=0.0, pivot_z=0.0, axis_x=0.0, axis_y=0.0, axis_z=0.0, flag=0):
-	"""Creates a constraint.
+    # type: (int, int, int, float, float, float, float, float, float, int) -> _types.KX_ConstraintWrapper
+    """Creates a constraint.
 
-	Parameters:	
-	physicsid_1 (int) - The physics id of the first object in constraint.
-	physicsid_2 (int) - The physics id of the second object in constraint.
-	constraint_type (int) - The type of the constraint, see Create Constraint Constants.
-	pivot_x (float) - Pivot X position. (optional)
-	pivot_y (float) - Pivot Y position. (optional)
-	pivot_z (float) - Pivot Z position. (optional)
-	axis_x (float) - X axis angle in degrees. (optional)
-	axis_y (float) - Y axis angle in degrees. (optional)
-	axis_z (float) - Z axis angle in degrees. (optional)
-	flag (int) - 128 to disable collision between linked bodies. (optional)
+    Args:
+        physicsid_1 (int): The physics id of the first object in constraint.
+        physicsid_2 (int): The physics id of the second object in constraint.
+        constraint_type (int): The type of the constraint, see Create Constraint Constants.
+        pivot_x (float): Pivot X position. (optional)
+        pivot_y (float): Pivot Y position. (optional)
+        pivot_z (float): Pivot Z position. (optional)
+        axis_x (float): X axis angle in degrees. (optional)
+        axis_y (float): Y axis angle in degrees. (optional)
+        axis_z (float): Z axis angle in degrees. (optional)
+        flag (int): 128 to disable collision between linked bodies. (optional)
 
-	Returns:	
-	A constraint wrapper.
+    Returns:
+        KX_ConstraintWrapper: A constraint wrapper."""
 
-	Return type:
-	KX_ConstraintWrapper"""
-	from .. import types
-	return types.KX_ConstraintWrapper()
-	
+    pass
+
 def createVehicle(physicsid):
-	"""Creates a vehicle constraint.
-	
-	Parameters:
-	physicsid (int) - The physics id of the chassis object in constraint.
-	
-	Returns:
-	A vehicle constraint wrapper.
-	
-	Return type:
-	KX_VehicleWrapper"""
-	from .. import types
-	return types.KX_VehicleWrapper
+    # type: (int) -> _types.KX_VehicleWrapper
+    """Creates a vehicle constraint.
+
+    Args:
+        physicsid (int): The physics id of the chassis object in constraint.
+
+    Returns:
+        KX_VehicleWrapper: A vehicle constraint wrapper."""
+
+    pass
 
 def exportBulletFile(filename):
-	"""Exports a file representing the dynamics world (usually using .bullet extension).
+    # type: (str) -> None
+    """Exports a file representing the dynamics world (usually using .bullet extension).
+    See Bullet binary serialization.
 
-	See Bullet binary serialization.
+    Args:
+        filename (str): File path."""
 
-	Parameters:
-	filename (str) - File path."""
-	pass
-	
+    pass
+
 def getAppliedImpulse(constraintId):
-	"""Parameters:
-	constraintId (int) - The id of the constraint.
+    # type: (int) -> float
+    """Args:
+        constraintId (int): The id of the constraint.
 
-	Returns: The most recent applied impulse.
+    Returns:
+        float: The most recent applied impulse."""
 
-	Return type: float"""
-	return True
-	
+    pass
+
 def getVehicleConstraint(constraintId):
-	"""Parameters:
-	constraintId (int) - The id of the vehicle constraint.
+    # type: (int) -> _types.KX_VehicleWrapper
+    """Args:
+        constraintId (int): The id of the vehicle constraint.
 
-	Returns: A vehicle constraint object.
+    Returns:
+        KX_VehicleWrapper: A vehicle constraint object."""
 
-	Return type: KX_VehicleWrapper"""
-	from .. import types
-	return types.KX_VehicleWrapper
-	
+    pass
+
 def getCharacter(gameobj):
-	"""Parameters:
-	gameobj (KX_GameObject) - The game object with the character physics.
+    # type: (_types.KX_GameObject) -> _types.KX_CharacterWrapper
+    """Args:
+        gameobj (KX_GameObject): The game object with the character physics.
 
-	Returns:	
-	Character wrapper.
+    Returns:
+        KX_CharacterWrapper: Character wrapper."""
 
-	Return type:
-	KX_CharacterWrapper"""
-	from .. import types
-	return types.KX_CharacterWrapper
-	
+    pass
+
 def removeConstraint(constraintId):
-	"""Removes a constraint.
+    # type: (int) -> None
+    """Removes a constraint.
 
-	Parameters:
-	constraintId (int) - The id of the constraint to be removed."""
-	pass
-	
+    Args:
+        constraintId (int): The id of the constraint to be removed."""
+
+    pass
+
 def setCcdMode(ccdMode):
-	"""Note: Very experimental, not recommended
+    # type: (int) -> None
+    """Sets the CCD (Continous Colision Detection) mode in the Physics Environment.
 
-	Sets the CCD (Continous Colision Detection) mode in the Physics Environment.
+    Args:
+        ccdMode (int): The new CCD mode.
 
-	Parameters:
-	ccdMode (int) - The new CCD mode."""
-	pass
-	
+    Note:
+        Very experimental, not recommended."""
+
+    pass
+
 def setContactBreakingTreshold(breakingTreshold):
-	"""Note: Reasonable default is 0.02 (if units are meters)
+    # type: (float) -> None
+    """Sets tresholds to do with contact point management.
 
-	Sets tresholds to do with contact point management.
+    Args:
+        breakingTreshold (float): The new contact breaking treshold.
 
-	Parameters:
-	breakingTreshold (float) - The new contact breaking treshold."""
-	pass
-	
+    Note:
+        Reasonable default is 0.02 (if units are meters)."""
+
+    pass
+
 def setDeactivationAngularTreshold(angularTreshold):
-	"""Sets the angular velocity treshold.
+    # type: (float) -> None
+    """Sets the angular velocity treshold.
 
-	Parameters:
-	angularTreshold (float) - New deactivation angular treshold."""
-	pass
-	
+    Args:
+        angularTreshold (float): New deactivation angular treshold."""
+
+    pass
+
 def setDeactivationLinearTreshold(linearTreshold):
-	"""Sets the linear velocity treshold.
+    # type: (float) -> None
+    """Sets the linear velocity treshold.
 
-	Parameters:
-	linearTreshold (float) - New deactivation linear treshold."""
-	pass
-	
+    Args:
+        linearTreshold (float): New deactivation linear treshold."""
+
+    pass
+
 def setDeactivationTime(time):
-	"""Sets the time after which a resting rigidbody gets deactived.
+    # type: (float) -> None
+    """Sets the time after which a resting rigidbody gets deactived.
 
-	Parameters:
-	time (float) - The deactivation time."""
-	pass
-	
+    Args:
+        time (float): The deactivation time."""
+
+    pass
+
 def setDebugMode(mode):
-	"""Sets the debug mode.
+    # type: (int) -> None
+    """Sets the debug mode.
 
-	Parameters:
-	mode (int) - The new debug mode, see Debug Mode Constants."""
-	pass
-	
+    Args:
+        mode (int): The new debug mode, see Debug Mode Constants."""
+
+    pass
+
 def setGravity(x, y, z):
-	"""Sets the gravity force.
+    # type: (float, float, float) -> None
+    """Sets the gravity force.
 
-	Parameters:
-	x (float) - Gravity X force.
-	y (float) - Gravity Y force.
-	z (float) - Gravity Z force."""
-	pass
-	
+    Args:
+        x (float): Gravity X force.
+        y (float): Gravity Y force.
+        z (float): Gravity Z force."""
+
+    pass
+
 def setLinearAirDamping(damping):
-	"""Note: Not implemented
+    # type: (float) -> None
+    """Sets the linear air damping for rigidbodies.
 
-	Sets the linear air damping for rigidbodies."""
-	pass
-	
+    Note:
+        Not implemented."""
+
+    pass
+
 def setNumIterations(numiter):
-	"""Sets the number of iterations for an iterative constraint solver.
+    # type: (int) -> None
+    """Sets the number of iterations for an iterative constraint solver.
 
-	Parameters:
-	numiter (int) - New number of iterations."""
-	pass
-	
+    Args:
+        numiter (int): New number of iterations."""
+
+    pass
+
 def setNumTimeSubSteps(numsubstep):
-	"""Sets the number of substeps for each physics proceed. Tradeoff quality for performance.
+    # type: (int) -> None
+    """Sets the number of substeps for each physics proceed. Tradeoff quality for performance.
 
-	Parameters:
-	numsubstep (int) - New number of substeps."""
-	pass
-	
+    Args:
+        numsubstep (int): New number of substeps."""
+
+    pass
+
 def setSolverDamping(damping):
-	"""Note: Very experimental, not recommended
+    # type: (float) -> None
+    """Sets the damper constant of a penalty based solver.
 
-	Sets the damper constant of a penalty based solver.
+    Args:
+        damping (float): New damping for the solver.
 
-	Parameters:
-	damping (float) - New damping for the solver."""
-	pass
-	
+    Note:
+        Very experimental, not recommended."""
+
+    pass
+
 def setSolverTau(tau):
-	"""Note: Very experimental, not recommended
+    # type: (float) -> None
+    """Sets the spring constant of a penalty based solver.
 
-	Sets the spring constant of a penalty based solver.
+    Args:
+        tau (float): New tau for the solver.
 
-	Parameters:
-	tau (float) - New tau for the solver."""
-	pass
-	
+    Note:
+        Very experimental, not recommended."""
+
+    pass
+
 def setSolverType(solverType):
-	"""Note: Very experimental, not recommended
+    # type: (int) -> None
+    """Sets the solver type.
 
-	Sets the solver type.
+    Args:
+        solverType (int): The new type of the solver.
 
-	Parameters:
-	solverType (int) - The new type of the solver."""
-	pass
-	
+    Note:
+        Very experimental, not recommended."""
+
+    pass
+
 def setSorConstant(sor):
-	"""Note Very experimental, not recommended
+    # type: (float) -> None
+    """Sets the successive overrelaxation constant.
 
-	Sets the successive overrelaxation constant.
+    Args:
+        sor (float): New sor value.
 
-	Parameters:
-	sor (float) - New sor value."""
-	pass
-	
+    Note:
+        Very experimental, not recommended."""
+
+    pass
+
 def setUseEpa(epa):
-	"""Note Not implemented"""
-	pass
+    # type: (bool) -> None
+    """Note:
+        Not implemented."""
+
+    pass
+
 
 ## Constants
 error = ""
+"""Symbolic constant string that indicates error."""
 
 # Debug Mode Constants
 DBG_NODEBUG = 0
+"""No debug."""
+
 DBG_DRAWWIREFRAME = 1
+"""Draw wireframe in debug."""
+
 DBG_DRAWAABB = 2
+"""Draw Axis Aligned Bounding Box in debug."""
+
 DBG_DRAWFREATURESTEXT = 4
+"""Draw features text in debug."""
+
 DBG_DRAWCONTACTPOINTS = 8
+"""Draw contact points in debug."""
+
 DBG_NOHELPTEXT = 32
+"""Debug without help text."""
+
 DBG_DRAWTEXT = 64
+"""Draw text in debug."""
+
 DBG_PROFILETIMINGS = 128
+"""Draw profile timings in debug."""
+
 DBG_ENABLESATCOMPARISION = 256
+"""Enable sat comparision in debug."""
+
 DBG_DISABLEBULLETLCP = 512
+"""Disable Bullet LCP."""
+
 DBG_ENABLECCD = 1024
+"""Enable Continous Collision Detection in debug."""
+
 DBG_DRAWCONSTRAINTS = 2048
+"""Draw constraints in debug."""
+
 DBG_DRAWCONSTRAINTSLIMITS = 4096
+"""Draw constraint limits in debug."""
+
 DBG_FASTWIREFRAME = 8192
+"""Draw a fast wireframe in debug."""
 
 # Create Constraints Constants
 POINTTOPOINT_CONSTRAINT = 1
